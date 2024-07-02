49e2bc22f444923da34cc4261ea77a4c0a380dee
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2024-07-02 17:23:23 -0400

    added detect input script

--- test_output.py
+++ test_output.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 
 # Set up the GPIO mode
-GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
+GPIO.setmode(GPIO.BOARD)  # Use BOARD pin numbering
 
-# Define all digital output pins (DO0 to DO15) based on BCM numbering
+# Define all digital output pins (DO0 to DO15) and VCC based on BOARD numbering
 output_pins = {
-    "DO0": 17, "DO1": 27, "DO2": 22, "DO3": 23,
-    "DO4": 24, "DO5": 10, "DO6": 9, "DO7": 25,
-    "DO8": 11, "DO9": 5, "DO10": 6, "DO11": 13,
-    "DO12": 19, "DO13": 26, "DO14": 21, "DO15": 20
+    "VCC": 1,  # Pin 1 for 3.3V
+    "DO0": 11, "DO1": 13, "DO2": 15, "DO3": 16,
+    "DO4": 18, "DO5": 19, "DO6": 21, "DO7": 22,
+    "DO8": 23, "DO9": 5, "DO10": 7, "DO11": 8,
+    "DO12": 10, "DO13": 12, "DO14": 3, "DO15": 24
 }
 
 # Set up output pins
