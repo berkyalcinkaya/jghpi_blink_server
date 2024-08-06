976e8c5d0d4525394e1ef30eda12acc22075d219
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2024-08-06 11:34:52 -0400

    modify blink test

--- blink_test.py
+++ blink_test.py
@@ -1,5 +1,6 @@
 from board_utils import send_command
 import time
+import sys
 
 def blink_pin(interval):
     """Blink the pin on and off with the given interval."""
@@ -14,5 +15,6 @@ def blink_pin(interval):
 
 if __name__ == "__main__":
     send_command("dio mode DO_G0 source")
-    blink_interval = 1  # Blink once a second
+    blink_interval = float(sys.argv[-1])  # Blink once a second
+    print("testing with interval", blink_interval)
     blink_pin(blink_interval)
