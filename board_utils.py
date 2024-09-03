947e7f057df7975e8d36998993562e1b32be502a
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2024-09-03 15:25:15 -0400

    ADD: switch

--- board_utils.py
+++ board_utils.py
@@ -36,9 +36,6 @@ class InPin():
         command = f"dio get {self.type}_G{self.group_num} input {self.pin_num}"
         response = self.ser.send_command(command, read=True)
         response_int = int(response.split("\n")[-2])
-
-        print(response_int)
-
         if response_int == 1:
             return True
         return False
